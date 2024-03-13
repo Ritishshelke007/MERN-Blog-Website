42d5a0931d78ccfb6b3ba2d74f5f6c8d0b72e99f
Ritishshelke007 <ritishshelke007@gmail.com>
2024-03-13 19:56:48 +0530

    Feature: Upload  on cloudinary

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // import routes
 import userRouter from "./routes/user.routes.js";
